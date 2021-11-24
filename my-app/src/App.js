1bb9be1203b191dd01bdf0314a3bf9eb685a0200
Katya Rozhko <49344755+KatyaRzh@users.noreply.github.com>
2021-11-23 16:23:48 -0800

    App.js published

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,22 @@
-import logo from './logo.svg';
-import './App.css';
+import "./styles.css";
+import Cities from "./Cities";
+import Form from "./Form";
+import CurrentCity from "./CurrentCity";
+import CurrentTemp from "./CurrentTemp";
+import HumidityWind from "./HumidityWind";
+import Crypto from "./Crypto";
+import Footer from "./Footer";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default function App() {
+	return (
+		<div className="App">
+			<Cities />
+			<Form />
+			<CurrentCity />
+			<CurrentTemp />
+			<HumidityWind />
+			<Crypto />
+			<Footer />
+		</div>
+	);
 }
-
-export default App;
