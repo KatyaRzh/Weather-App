d40e5e4781a076cc2c07e88768b6e52250b13894
Katya Rozhko <49344755+KatyaRzh@users.noreply.github.com>
2021-11-24 14:08:55 -0800

    formattedDate component published

--- src/Weather.js
+++ src/Weather.js
@@ -2,11 +2,13 @@ import React, { useState } from "react";
 import axios from "axios";
 import WeatherInfo from "./Weatherinfo";
 
-export default function Weather() {
+export default function Weather(props) {
 	const [weatherData, setWeatherData] = useState({ ready: false });
+	const [city, setCity] = useState(props.defaultCity);
 	function handleResponse(response) {
 		setWeatherData({
 			ready: true,
+			date: new Date(response.data.dt * 1000),
 			temperature: Math.round(response.data.main.temp),
 			wind: Math.round(response.data.wind.speed),
 			humidity: Math.round(response.data.main.humidity),
@@ -17,10 +19,25 @@ export default function Weather() {
 		});
 	}
 
+	function search() {
+		const apiKey = "b111d9ecb27df9d5eac99c7ddb03f247";
+		let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+		axios.get(apiUrl).then(handleResponse);
+	}
+
+	function handleSubmit(event) {
+		event.preventDefault();
+		search(city);
+	}
+
+	function handleCityChange(event) {
+		setCity(event.target.value);
+	}
+
 	if (weatherData.ready) {
 		return (
 			<div className="Weather">
-				<form>
+				<form onSubmit={handleSubmit}>
 					<div className="row">
 						<div className="col-9">
 							<input
@@ -28,6 +45,7 @@ export default function Weather() {
 								placeholder="Enter a city.."
 								className="form-control"
 								autoFocus="on"
+								onChange={handleCityChange}
 							/>
 						</div>
 						<div className="col-3">
@@ -43,10 +61,7 @@ export default function Weather() {
 			</div>
 		);
 	} else {
-		const apiKey = "b111d9ecb27df9d5eac99c7ddb03f247";
-		let apiUrl =
-			"https://api.openweathermap.org/data/2.5/weather?q=${props.defaultCity}&appid=${apiKey}&units=metric";
-		axios.get(apiUrl).then(handleResponse);
+		search();
+		return "Loading...";
 	}
-	return "loading...";
 }
--- src/Weatherinfo.js
+++ src/Weatherinfo.js
@@ -1,38 +1,48 @@
-import react from "react";
+import React from "react";
+import FormattedDate from "./FormattedDate";
 
-export default WeatherInfo(props) {
-  return (
-    <div className="WeatherInfo">
-      if (loaded) {
-		return (
-      <div>
-        <h1>{weatherData.city}<h1/>
+export default function WeatherInfo(props) {
+	return (
+		props.loaded && (
+			<div className="WeatherInfo">
+				<div>
+					<h1>{props.weatherData.city}</h1>
 
-        <ul class ="weather-details">
-        <li><FormattedDate date={weatherData.date}/></li>
-        <li className="text-capitalize">{weatherData.description}</li>
-        </ul>
-        <div className="row mt-3">
-        <div className="col-6">
-        <div className="clearfix">
-        <img src={props.Data.iconUrl}
-        alt={props.data.description}
-        className="float-left"/>
-        <div className="float-left">
-        <span className="temperature">
-      {Math.round(weatherData.temperature)}
-        </span>
-        <span className="unit">C</span>
-        </div>
-        </div>
-        </div>
-        <div className="col-6">
-        <ul>
-        <li>Humidity: 40%</li>
-        <li>Wind: 0.45 km/h</li>
-        </ul>
-        </div>
-        </div>
-      </div>
-
-
+					<ul class="weather-details">
+						<li>
+							<FormattedDate date={props.weatherData.date} />
+						</li>
+						<li className="text-capitalize">
+							{props.weatherData.description}
+						</li>
+					</ul>
+					<div className="row mt-3">
+						<div className="col-6">
+							<div className="clearfix">
+								<img
+									src={props.Data.iconUrl}
+									alt={props.data.description}
+									className="float-left"
+								/>
+								<div className="float-left">
+									<span className="temperature">
+										{Math.round(
+											props.weatherData.temperature
+										)}
+									</span>
+									<span className="unit">C</span>
+								</div>
+							</div>
+						</div>
+						<div className="col-6">
+							<ul>
+								<li>Humidity: 40%</li>
+								<li>Wind: 0.45 km/h</li>
+							</ul>
+						</div>
+					</div>
+				</div>
+			</div>
+		)
+	);
+}
