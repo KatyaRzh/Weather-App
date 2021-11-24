84d3d7cdc633fb698e418ff49f8ee7e43a0a28f5
Katya Rozhko <49344755+KatyaRzh@users.noreply.github.com>
2021-11-23 16:25:36 -0800

    Cities.js published

---
+++ my-app/src/Cities.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function Cities() {
+	return (
+		<div class="overview">
+			<h1 id="searching-for-city">Weather Today</h1>
+
+			<ul class="menu">
+				<li>San Francisco</li>
+				<li>Cupertino</li>
+				<li>Mexico City</li>
+				<li>Kryvyi Rih</li>
+			</ul>
+		</div>
+	);
+}
