26bdb925849cb64aa5833e7ab311a43e6662f002
Katya Rozhko <49344755+KatyaRzh@users.noreply.github.com>
2021-11-25 20:07:22 -0800

    fixed bugs

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
 import "./index.css";
 import Cities from "./Cities";
-import CurrentCity from "./CurrentCity";
 import WeatherTemperature from "./Weathertemperature.js.js";
 import Crypto from "./Crypto";
 import Footer from "./Footer";
 import Weather from "./Weather";
-import Weatherinfo from "./Weatherinfo";
 
 export default function App() {
 	return (
@@ -13,8 +11,8 @@ export default function App() {
 			<div className="App">
 				<Cities />
 				<Weather defaultCity="New York" />
-				<CurrentCity />
 				<WeatherTemperature />
+
 				<Crypto />
 				<Footer />
 			</div>
